6db45922b971882dee7f3e63f2820c06dd3a972e
DXelor <dzaliaspro@gmail.com>
2020-11-18 19:43:57 -0500

    Controladores y rutas en Node

--- backend/app.js
+++ backend/app.js
@@ -6,26 +6,14 @@ var bodyParser = require('body-parser');
 var app = express();
 
 //CARGAR RUTAS
+var project_routes = require('./routes/project');
 
 //MIDDLEWARE
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-//CORS
-
 //RUTAS
-app.get('/',(req,res)=>{
-    res.status(200).send(
-        "<h1>pagina de inicio</h1>"
-    );
-});
-app.post('/test',(req,res)=>{
-    console.log(req.body.nombre);
-    console.log(req.body.apellido);
-    res.status(200).send({
-        message: "API NodeJS"
-    })
-});
+app.use('/api', project_routes);
 
 //EXPORTAR
 module.exports = app;
---
+++ backend/routes/project.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const { Router } = require('express');
+var express = require('express');
+var ProjectController = require('../controllers/project');
+
+var router = express.Router();
+
+router.get('/home', ProjectController.home);
+router.post('/test', ProjectController.test);
+
+module.exports = router;
